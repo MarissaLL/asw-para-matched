ddeadd9206de0bc1ae024eef17496688a6cacb2d
Marissa Le Lec <marissalelec@gmail.com>
2018-09-03 16:44:58 +1200

    updated sampling positions, tidied formatting

--- src/sample_locations_map.R
+++ src/sample_locations_map.R
@@ -5,25 +5,40 @@ library(maps)
 # Load in map from maps package
 nz_map <- map_data("nz")
 
-# Define sampling location points. These are currently based on locations of Hamilton, Lincoln and Mosgiel, not precise locations.
+# Define sampling location points. 
 sampling_loc <-  data.frame(
-  long = c(175.32939, 172.48654, 170.34754),
-  lat = c(-37.78259,-43.63980, -45.87758),
+  long = c(175.310, 172.472, 170.348),
+  lat = c(-37.774,-43.634, -45.878),
   names = c('Ruakura', 'Lincoln', 'Invermay'),
   stringsAsFactors = FALSE)  
 
 # Manually specify label position to make it better
-lab_pos <- c(1.4, -0.5, -0.3)
+lab_pos <- c(1.6, -0.8, -0.5)
 
 # Plot the map
 # axis lines on top and right currently not working
 ggplot() + 
-  geom_polygon(data = nz_map, aes(x=long, y = lat, group = group), fill = NA, colour = 'black' ) + 
-  coord_fixed(1.45) + 
-  geom_point(data = sampling_loc, aes(x = long, y = lat), colour = 'red', size = 3.5) +
-  geom_text(data = sampling_loc, aes(x = long, y = lat, label = names), hjust= lab_pos) +
+  geom_polygon(data = nz_map, 
+               mapping = aes(x=long, 
+                             y = lat, 
+                             group = group), 
+               fill = NA, colour = 'black' ) + 
+  coord_fixed(1.4) + 
+  geom_point(data = sampling_loc, 
+             mapping = aes(x = long, 
+                           y = lat), 
+             colour = 'red', 
+             size = 3.5) +
+  geom_text(data = sampling_loc, 
+            mapping = aes(x = long, 
+                          y = lat, 
+                          label = names), 
+            hjust= lab_pos) +
   theme_classic() + 
-  theme(axis.line.x.top = element_line(), axis.line.y.right = element_line()) +
-  labs(x = 'Longitude', y = 'Latitude')
-
-  
+  theme(axis.line.x.top = element_line(), 
+        axis.line.y.right = element_line(), 
+        panel.background = element_rect(fill = NA, 
+                                        colour = "black", 
+                                        size = 0.7 )) +
+  labs(x = 'Longitude', 
+       y = 'Latitude')
